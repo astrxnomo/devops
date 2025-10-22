b1a0f6986594dd53bfd3d09461d2dbacd1b8858b
Felipe Giraldo <luisgir827@gmail.com>
2025-10-22 00:41:27 +0000

    Agregar flujo de trabajo de construcción de imagen y pruebas para la aplicación Flask

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/suma', methods=['GET'])
+def suma():
+  a = int(request.args.get('a',0))
+  b = int(request.args.get('b',0))
+
+  result = a + b
+
+  return jsonify({'resultad': result})
+
+@app.route('/multiplica', methods=['POST'])
+def multiplica():
+  datos = request.get_json()
+  a = datos.get('a', 1)
+  b = datos.get('b', 1)
+
+  result = a * b
+
+  return jsonify({'resultado': result})
